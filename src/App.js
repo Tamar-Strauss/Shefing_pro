d428dfd51303797822eb1f3a60fbc8e894e621ac
Tamar Strauss <103378537+Tamar-Strauss@users.noreply.github.com>
2024-01-07 02:05:04 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+
+// import { DataView } from '../src/Componnent/DataView.js';
+import UserList from '../src/Componnent/UserList.js';
+import UserPosts from './Componnent/UserPosts.js';
+
+export default function Shefing_pro() {
+  const [selectRow, setSelectRow] = useState(null);
+  
+  return (<>
+    <UserList selectRow={selectRow} setSelectRow={setSelectRow} ></UserList>
+    {selectRow && <UserPosts selectRow={selectRow} ></UserPosts> }
+    </>
+  )
+}
